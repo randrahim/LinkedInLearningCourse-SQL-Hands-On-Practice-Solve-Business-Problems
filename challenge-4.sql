f288ec771db33a1809ec3ddfe23ed1f128931c10
rand.rahim <rand.rahim@outlook.com>
2026-06-03 23:17:51 -0400

    challenge 4

--- challenge-4.sql
+++ challenge-4.sql
@@ -10,12 +10,12 @@ SELECT subs.subscriptionid,
        case 
         when maxstatus = 1 then 'PaymentWidgetOpened'
         when maxstatus = 2 then 'PaymentEntered'
-        when maxstatus = 3 and currentstatus = 0 then 'PaymentEntered'
+        when maxstatus = 3 and currentstatus = 0 then 'User Enter'
         when maxstatus = 3 and currentstatus != 0 then 'PaymentEntered'
         when maxstatus = 4 and currentstatus = 0 then 'PaymentEntered'
         when maxstatus = 4 and currentstatus != 0 then 'PaymentEntered'
         when maxstatus = 5 then 'Complete'
-        when maxstatus is null then 'User did not st'
+        when maxstatus is null then 'User did not start payment'
        end as paymentfunnelstage
 FROM subscriptionid subs 
 LEFT JOIN 
