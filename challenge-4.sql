61881807423ef8e9a30326b7e6e26440390ce3ff
rand.rahim <rand.rahim@outlook.com>
2026-06-03 23:13:09 -0400

    challenge 4

---
+++ challenge-4.sql
@@ -0,0 +1,29 @@
+with max_status_reached as (
+  SELECT subscriptionid,
+         max(statusid) as MaxStatus 
+  FROM paymentstatuslog
+  GROUP BY subscriptionid
+),
+
+paymentfunnelstages as (
+SELECT subs.subscriptionid,
+       case 
+        when maxstatus = 1 then 'PaymentWidgetOpened'
+        when maxstatus = 2 then 'PaymentEntered'
+        when maxstatus = 3 and currentstatus = 0 then 'PaymentEntered'
+        when maxstatus = 3 and currentstatus != 0 then 'PaymentEntered'
+        when maxstatus = 4 and currentstatus = 0 then 'PaymentEntered'
+        when maxstatus = 4 and currentstatus != 0 then 'PaymentEntered'
+        when maxstatus = 5 then 'Complete'
+        when maxstatus is null then 'User did not st'
+       end as paymentfunnelstage
+FROM subscriptionid subs 
+LEFT JOIN 
+    max_status_reached m 
+ON subs.subscriptionid = m.subscriptionid 
+)
+
+SELECT paymentfunnelstage, 
+       count(subscriptionid) as subscriptions 
+FROM paymentfunnelstages 
+GROUP BY paymentfunnelstage;
