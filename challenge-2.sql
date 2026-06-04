0d35467f59d973b159f50e0bbc7d2ff0857aead2
rand.rahim <rand.rahim@outlook.com>
2026-06-03 22:03:34 -0400

    challenge 2

--- challenge-2.sql
+++ challenge-2.sql
@@ -1,3 +1,4 @@
+with monthly_revs as (
 SELECT *
 FROM subscriptions s
 
@@ -5,3 +6,18 @@ JOIN products p
 ON s.productid = p.productid
 
 WHERE s.orderdate between '2022-01-01' and '2022-12-31'
+
+GROUP BY 
+  date_trunc('month', s.orderdate), p.productname
+)
+
+SELECT 
+  productname,
+  min(revenue) as min_rev,
+  max(revenue) as max_rev,
+  avg(revenue) as avg_rev,
+  stddev(revenue) as std_dev_rev
+FROM 
+  monthly_revs
+GROUP BY 
+  productname;
