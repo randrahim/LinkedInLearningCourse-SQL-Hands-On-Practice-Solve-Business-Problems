88b040fbdb1b98af4b823f3ab2ae1a3175c74cf4
rand.rahim <rand.rahim@outlook.com>
2026-06-05 15:07:56 -0400

    challenge 10

---
+++ challenge-10.sql
@@ -0,0 +1,16 @@
+with monthly_revs as (
+  SELECT 
+    date_trunc('month', orderdate) as order_month,
+    sum(revenue) as monthly_revenue
+  FROM subscriptions
+  GROUP BY date_trunc('month', orderdate)
+)
+
+SELECT current.order_month as current_month,
+       previous.order_month as previous_month,
+       current.monthly_revenue as current_revenue,
+       previous.monthly_revenue as previous_revenue
+FROM monthly_revs current
+JOIN monthly_revs previous
+WHERE current.monthly_revenue > previous.monthly_revenue
+AND datediff('month', previous.order_month, current.order_month) = 1;
