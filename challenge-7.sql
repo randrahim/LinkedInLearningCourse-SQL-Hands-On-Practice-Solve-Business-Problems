76cfdb456466e015d7ae2e1f043ee45128ff071e
rand.rahim <rand.rahim@outlook.com>
2026-06-05 14:22:43 -0400

    challenge 7

---
+++ challenge-7.sql
@@ -0,0 +1,20 @@
+with all_subscriptions as (
+  SELECT subscriptionid,
+         expirationdate
+  FROM subscriptionsproduct1
+  WHERE active = 1
+
+  UNION ALL 
+  
+  SELECT subscriptionid,
+         expirationdate
+  FROM subscriptionsproduct2
+  WHERE active = 1
+)
+
+SELECT 
+  date_trunc('year', expirationdate) as exp_year,
+  count(*) as subscriptions 
+FROM all_subscriptions 
+GROUP BY date_trunc('year', expirationdate)
+
