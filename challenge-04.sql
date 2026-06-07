c81a26dec8acd691c1f7f7348b18b3bd95f78361
rand.rahim <rand.rahim@outlook.com>
2026-06-07 14:58:14 -0400

    challenge 03 -edit

--- challenge-04.sql
+++ challenge-04.sql
@@ -1,29 +1,31 @@
-with max_status_reached as (
-  SELECT subscriptionid,
-         max(statusid) as MaxStatus 
+WITH max_status_reached AS (
+  SELECT
+    subscriptionid,
+    MAX(statusid) AS maxstatus
   FROM paymentstatuslog
   GROUP BY subscriptionid
 ),
 
-paymentfunnelstages as (
-SELECT subs.subscriptionid,
-       case 
-        when maxstatus = 1 then 'PaymentWidgetOpened'
-        when maxstatus = 2 then 'PaymentEntered'
-        when maxstatus = 3 and currentstatus = 0 then 'User Enter'
-        when maxstatus = 3 and currentstatus != 0 then 'PaymentEntered'
-        when maxstatus = 4 and currentstatus = 0 then 'PaymentEntered'
-        when maxstatus = 4 and currentstatus != 0 then 'PaymentEntered'
-        when maxstatus = 5 then 'Complete'
-        when maxstatus is null then 'User did not start payment'
-       end as paymentfunnelstage
-FROM subscriptionid subs 
-LEFT JOIN 
-    max_status_reached m 
-ON subs.subscriptionid = m.subscriptionid 
+paymentfunnelstages AS (
+  SELECT
+    subs.subscriptionid,
+    CASE 
+      WHEN m.maxstatus = 1 THEN 'PaymentWidgetOpened'
+      WHEN m.maxstatus = 2 THEN 'PaymentEntered'
+      WHEN m.maxstatus = 3 AND subs.currentstatus = 0 THEN 'User Error with Payment Submission'
+      WHEN m.maxstatus = 3 AND subs.currentstatus <> 0 THEN 'Payment Submitted'
+      WHEN m.maxstatus = 4 AND subs.currentstatus = 0 THEN 'Payment Processing Error with Vendor'
+      WHEN m.maxstatus = 4 AND subs.currentstatus <> 0 THEN 'Payment Success'
+      WHEN m.maxstatus = 5 THEN 'Complete'
+      WHEN m.maxstatus IS NULL THEN 'User did not start payment process'
+    END AS paymentfunnelstage
+  FROM subscriptions subs
+  LEFT JOIN max_status_reached m
+    ON subs.subscriptionid = m.subscriptionid
 )
 
-SELECT paymentfunnelstage, 
-       count(subscriptionid) as subscriptions 
-FROM paymentfunnelstages 
+SELECT
+  paymentfunnelstage,
+  COUNT(subscriptionid) AS subscriptions
+FROM paymentfunnelstages
 GROUP BY paymentfunnelstage;
