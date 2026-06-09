92f828d1989c69a9b32c1712139859b56e3e768a
rand.rahim <rand.rahim@outlook.com>
2026-06-09 13:58:46 -0400

    challenge 08 -edit

--- challenge-09.sql
+++ challenge-09.sql
@@ -1,8 +1,9 @@
 SELECT 
-    employees.employeeid,
-    employees.name as employee_name,
-    manager.name as manager_name,
-    manager.email as manager_email
-FROM employees
-LEFT JOIN employees manager
-ON employees.managerid = manager.employeeid;
+  e.employeeid,
+  e.name AS employee_name,
+  m.name AS manager_name,
+  COALESCE(m.email, e.email) AS contact_email
+FROM employees e
+LEFT JOIN employees m
+  ON e.managerid = m.employeeid
+WHERE e.department = 'Sales';
